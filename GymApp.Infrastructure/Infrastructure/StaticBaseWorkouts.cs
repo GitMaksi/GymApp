75809ab654a38f533bb3f1611172f68a139bfbc9
Maks Sudoł <maksssudol@gmail.com>
2025-07-11 23:34:44 +0200

    Mega fajne zmiankysie

---
+++ GymApp.Infrastructure/Infrastructure/StaticBaseWorkouts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.Infrastructure.Infrastructure
+{
+    public static class StaticBaseWorkouts
+    {
+        public static readonly Dictionary<string, List<string>> Days = new()
+        {
+            {
+                "ChestShoulders", new List<string>
+                {
+                    "Wyciskanie sztangi", "Wyciskanie hantli", "Rozpiętki", "Wyciskanie na barki"
+                }
+            },
+            {
+                "BackBiceps", new List<string>
+                {
+                    "Podciąganie", "Wiosłowanie sztangą", "Uginanie na biceps", "Face pull"
+                }
+            },
+            {
+                "LegsAbsTriceps", new List<string>
+                {
+                    "Przysiady", "Martwy ciąg", "Brzuszki", "Wyciskanie francuskie"
+                }
+            }
+        };
+    }
+}
